0727b39a40670f6118bb32a6ef3de77b2dfb2813
David Muckle <dvdmuckle@dvdmuckle.xyz>
2017-10-21 21:40:51 -0400

    Start of main function, includes argument input

--- bellnum.go
+++ bellnum.go
@@ -1,7 +1,23 @@
-package bellnum
+package main
 
 import "fmt"
+import "os"
+import "strconv"
+
+func bellnum(bellstart int) (belltotal int) {
+	return 0
+}
 
 func main() {
-	fmt.Println("vim-go")
+	if len(os.Args) < 2 {
+		fmt.Println("Please input a number")
+		return
+	}
+	bellstart, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	bellend := bellnum(bellstart)
+	fmt.Printf("The Bell number of %d is %d\n", bellstart, bellend)
 }
