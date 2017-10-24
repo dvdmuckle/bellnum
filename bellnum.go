e6289d3ebef995b2dc2ccc6a43ce337832f62ce4
David Muckle <dvdmuckle@dvdmuckle.xyz>
2017-10-23 20:21:03 -0400

    Conceptualizing algorithm

--- bellnum.go
+++ bellnum.go
@@ -4,11 +4,21 @@ import "fmt"
 import "os"
 import "strconv"
 
+// Wraper function for real function
 func Bellnum(bellIn int) (belltotal int) {
+	//Handle the base case to avoid any unecessary computation
 	if bellIn <= 1 {
 		return 1
 	}
-	return 2
+	var bellSlice [][]int
+	bellSlice[0][0] = 1
+	bellFinal := bellcurse(bellIn, bellSlice)
+	return bellFinal[len(bellFinal)-1][len(bellFinal)-1]
+}
+
+// Actual function that recursively goes through Bell triangle
+func bellcurse(bellIn int, bellArrIn [][]int) (bellArrOut [][]int) {
+	return nil
 }
 
 func main() {
